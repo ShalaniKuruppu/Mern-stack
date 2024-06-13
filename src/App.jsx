9bb8f61e2e038eb943dfbb6d1b62963f23a6e9bf
Shalani Kuruppu <shalanikuruppu08@gmail.com>
2024-06-13 14:12:54 +0530

    feature about

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/Signin';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
+import Header from './components/Header';
+
+
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+       <Header/>
+      <Routes>
+
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+
+      </Routes>
+    </BrowserRouter>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+        <h1 className='font-bold text-sm '>
+            <span className='text-state-500'>Sahand</span>
+            <span className='text-state-700'>Estate</span>
+        </h1>
+    </header>
+  )
+
+  <!-  first 31 minutes watched->
+}
